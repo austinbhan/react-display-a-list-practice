d18d9b826c4fb6679e4c708b448b1b3cf3af5e2c
austinbhan <austinbhan@outlook.com>
2022-06-08 11:44:44 -0700

    ItemOne.js

--- src/ItemOne.js
+++ src/ItemOne.js
@@ -1,1 +1,9 @@
+import ListOne from './ListOne';
 
+export default function ItemOne({ colors }) {
+  return <div>
+    {
+      colors.map((color, i) => <ListOne color={ color } key={color + 1} />)
+    }
+  </div>;
+}
--- src/ListOne.js
+++ src/ListOne.js
@@ -1,4 +1,4 @@
-export default function listOne({ color }) {
+export default function ListOne({ color }) {
     return <div>
         { color }
     </div>;
