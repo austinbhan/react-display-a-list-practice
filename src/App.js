3754ac2dcae0a1cf27521e0cf3c655553bc62469
austinbhan <austinbhan@outlook.com>
2022-06-08 12:52:26 -0700

    Working second array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import ColorsList from './ColorsList';
 import { colors } from './colors.js';
+
+import GamesList from './GamesList';
+import { games } from './games-array';
 // import your arrays here
 
 function App() {
   return (
     <div className="App">
       <ColorsList colors={colors} />
+      <GamesList games={ games } />
     </div>
   );
 }
