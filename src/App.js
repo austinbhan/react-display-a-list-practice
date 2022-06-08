3004c4a44a9de0f4beec5f98a4f273ec76da23d9
austinbhan <austinbhan@outlook.com>
2022-06-08 16:11:11 -0700

    Finish project?

--- src/App.js
+++ src/App.js
@@ -7,14 +7,19 @@ import { games } from './games-array';
 
 import ClassList from './ClassList';
 import { classroom } from './classroom.js';
+
+import RecipesList from './RecipesList';
+import { recipes } from './recipes.js';
+
 // import your arrays here
 
 function App() {
   return (
     <div className="App">
       <ColorsList colors={colors} />
-      <GamesList games={ games } />
+      <GamesList games={games} />
       <ClassList classroom={classroom} />
+      <RecipesList recipes={recipes}/>
     </div>
   );
 }
