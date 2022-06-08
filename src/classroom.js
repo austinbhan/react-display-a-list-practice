8b29a5f283635783a46d3134d5374d4a872deedf
austinbhan <austinbhan@outlook.com>
2022-06-08 14:22:46 -0700

    Created classroom array

---
+++ src/classroom.js
@@ -0,0 +1,32 @@
+export const classroom = [
+  { name: 'Taft',
+    about: {
+      age: '6',
+      grade: '1'
+    }
+  },
+  { name: 'Wilson',
+    about: {
+      age: '7',
+      grade: '2'
+    }
+  },
+  { name: 'Monroe',
+    about: {
+      age: '8',
+      grade: '3'
+    }
+  },
+  { name: 'Fillmore',
+    about: {
+      age: '9',
+      grade: '4'
+    }
+  },
+  { name: 'Adams',
+    about: {
+      age: '11',
+      grade: '6'
+    }
+  },
+];
